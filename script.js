d0e0d279722881d12ba2fc10d6cdad1d2ec49662
CadenLS <csinclair@student.neumont.edu>
2025-05-12 00:43:40 -0600

    try flipping

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 $(document).ready(function () {
-  $('#book').turn({
+  const $book = $('#book');
+
+  $book.turn({
     width: 800,
     height: 500,
     autoCenter: true,
     elevation: 50,
     gradients: true,
     duration: 1200
+  });
+
+  $('#nextPage').click(() => {
+    $book.turn('next');
+  });
+
+  $('#prevPage').click(() => {
+    $book.turn('previous');
   });
 });
