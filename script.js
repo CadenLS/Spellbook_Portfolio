b5d53900fc2393bc9046a96d000556257197ea5c
CadenLS <csinclair@student.neumont.edu>
2025-05-12 00:25:14 -0600

    Pages

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('#book').turn({
+    width: 800,
+    height: 500,
+    autoCenter: true,
+    elevation: 50,
+    gradients: true,
+    duration: 1200, // smooth page flip
+  });
+});
